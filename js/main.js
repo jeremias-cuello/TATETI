004da875412377ebd9b59d9894d3438de0b8556a
Jeremias0901 <cuellojeremiasnatanaelgmail.com>
2022-07-29 01:25:09 -0300

    borrado de main.js

--- js/main.js
+++
@@ -1,5 +0,0 @@
-var time = setInterval(() => {
-    
-    document.body.style.background ? (state) `repeating-linear-gradient(var(--colorBody_2), var(--colorBody_1))` : `repeating-linear-gradient(var(--colorBody_1), var(--colorBody_2))`;
-
-}, 1000);
